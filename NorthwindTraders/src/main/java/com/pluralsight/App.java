e81e0b86dcb7db01537a2bcca30fdd84293caf91
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-09 16:25:29 -0500

    Complete Exerecise 3: Prepared Statement

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -1,10 +1,16 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
 
+    static Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        System.out.print("Username: root\nPassword: ");
+        String password = userInput.nextLine();
 
         // 1. open a connection to the database
         // use the database URL to point to the correct database
@@ -14,29 +20,39 @@ public class App {
             connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/northwind",
                     "root",
-                    "yearup");
+                    password);
 
         // create statement
         // the statement is tied to the open connection
-        Statement statement = connection.createStatement();
-        
-        // define your query
-        String query = """
-                SELECT
-                    ProductName
-                FROM
-                    products
-                ORDER BY
-                    ProductName
-                """;
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                """
+                    SELECT
+                        ProductID
+                        , ProductName
+                        , UnitPrice
+                        , UnitsInStock
+                    FROM
+                        products
+                    """
+        );
 
         // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
+        ResultSet results = preparedStatement.executeQuery();
+
+        // table header
+        System.out.print("""
+             ID      Name                               Price   Stock
+             ---     --------------------------         ------  ------
+             """);
 
         // process the results
         while (results.next()) {
-            String productName = results.getString("ProductName");
-            System.out.println(productName);
+            // left aligned by however many spaces to align with table header above
+            System.out.printf("%-7d %-34s %-7.2f %-6d\n",
+                    results.getInt("ProductID"),
+                    results.getString("ProductName"),
+                    results.getDouble("UnitPrice"),
+                    results.getInt("UnitsInStock"));
         }
         // 3. Close the connection
         connection.close();
