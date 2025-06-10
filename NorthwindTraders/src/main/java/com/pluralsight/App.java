678625f3211c6f1aa3e52b25362a3097616d304e
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-10 15:43:15 -0500

    Complete Exercise 4: try with resources

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -7,8 +7,6 @@ public class App {
 
     static Scanner userInput = new Scanner(System.in);
     static Connection connection = null;
-    static PreparedStatement preparedStatement = null;
-    static ResultSet resultSet = null;
 
     public static void main(String[] args) {
 
@@ -21,12 +19,13 @@ public class App {
 
             // displays user options
             System.out.print("""
-                
-                What do you want to do?
-                    1) Display all products
-                    2) Display all customers
-                    0) Exit
-                """);
+                    
+                    What do you want to do?
+                        1) Display all products
+                        2) Display all customers
+                        3) Display all categories
+                        0) Exit
+                    """);
             System.out.print("Select an option: ");
 
             // stores user option for switch
@@ -38,8 +37,11 @@ public class App {
                 case 2:
                     displayCustomers();
                     break;
+                case 3:
+                    displayCategories();
+                    break;
                 case 0:
-                    closeResources();
+                    closeConnection();
                     homeScreen = false;
                     break;
                 default:
@@ -62,46 +64,60 @@ public class App {
                     "root",
                     password);
         } catch (SQLException e) {
-            System.out.println("Error with connection: " + e.getMessage());
+            System.out.println("Error with opening connection: " + e.getMessage());
             return null;
+        }
+    }
+
+    // closes the connection to database
+    public static void closeConnection() {
+
+        // closes the connection to database if not null
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Error with closing connection: " + e.getMessage());
+            }
         }
     }
 
     // displays all products from products table
     public static void displayProducts() {
 
-        try {
-            // creates prepared statement with the connection and writes the query
-            preparedStatement = connection.prepareStatement(
-                    """
-                        SELECT
-                            ProductID
-                            , ProductName
-                            , UnitPrice
-                            , UnitsInStock
-                        FROM
-                            products
+        try (
+                // creates prepared statement with the connection and writes the query
+                PreparedStatement preparedStatement1 = connection.prepareStatement(
                         """
-            );
+                                SELECT
+                                    ProductID
+                                    , ProductName
+                                    , UnitPrice
+                                    , UnitsInStock
+                                FROM
+                                    products
+                                """
+                );
 
-            // takes prepared statement with query to execute
-            resultSet = preparedStatement.executeQuery();
+                // takes prepared statement with query to execute
+                ResultSet resultSet1 = preparedStatement1.executeQuery();
 
+        ) {
             // table header
             System.out.print("""
-             
-             ID      Name                               Price   Stock
-             ---     --------------------------         ------  ------
-             """);
+                    
+                    ID      Name                               Price   Stock
+                    ---     --------------------------         ------  ------
+                    """);
 
             // displays the query executed
-            while (resultSet.next()) {
+            while (resultSet1.next()) {
                 // left aligned by however many spaces to align with table header above
                 System.out.printf("%-7d %-34s %-7.2f %-6d\n",
-                        resultSet.getInt("ProductID"),
-                        resultSet.getString("ProductName"),
-                        resultSet.getDouble("UnitPrice"),
-                        resultSet.getInt("UnitsInStock"));
+                        resultSet1.getInt("ProductID"),
+                        resultSet1.getString("ProductName"),
+                        resultSet1.getDouble("UnitPrice"),
+                        resultSet1.getInt("UnitsInStock"));
             }
 
         } catch (SQLException e) {
@@ -112,42 +128,42 @@ public class App {
     // displays all customers
     public static void displayCustomers() {
 
-        try {
-            // creates prepared statement with the connection and writes the query
-            preparedStatement = connection.prepareStatement(
-                    """
-                        SELECT
-                            ContactName
-                            , CompanyName
-                            , City
-                            , Country
-                            , Phone
-                        FROM
-                            customers
-                        WHERE
-                            Country IS NOT NULL
-                        ORDER BY
-                            Country
+        try (
+                // creates prepared statement with the connection and writes the query
+                PreparedStatement preparedStatement1 = connection.prepareStatement(
                         """
-            );
-            // executes the query from the prepared statement
-            resultSet = preparedStatement.executeQuery();
-
+                                SELECT
+                                    ContactName
+                                    , CompanyName
+                                    , City
+                                    , Country
+                                    , Phone
+                                FROM
+                                    customers
+                                WHERE
+                                    Country IS NOT NULL
+                                ORDER BY
+                                    Country
+                                """
+                );
+                // executes the query from the prepared statement
+                ResultSet resultSet1 = preparedStatement1.executeQuery();
+        ) {
             // displays query results from column names
-            while(resultSet.next()) {
+            while (resultSet1.next()) {
                 System.out.printf("""
-                        
-                        Contact Name: %s
-                        Company Name: %s
-                        City:         %s
-                        Country:      %s
-                        Phone:        %s
-                        """,
-                        resultSet.getString("ContactName"),
-                        resultSet.getString("CompanyName"),
-                        resultSet.getString("City"),
-                        resultSet.getString("Country"),
-                        resultSet.getString("Phone"));
+                                
+                                Contact Name: %s
+                                Company Name: %s
+                                City:         %s
+                                Country:      %s
+                                Phone:        %s
+                                """,
+                        resultSet1.getString("ContactName"),
+                        resultSet1.getString("CompanyName"),
+                        resultSet1.getString("City"),
+                        resultSet1.getString("Country"),
+                        resultSet1.getString("Phone"));
             }
 
         } catch (SQLException e) {
@@ -155,33 +171,106 @@ public class App {
         }
     }
 
-    // closes all resources
-    public static void closeResources() {
-        try {
-            // close the resources if not null
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+    // displays all categories
+    public static void displayCategories() {
+
+        // eats leftover
+        userInput.nextLine();
+
+        try (
+                // creates prepared statement and query for category table
+                PreparedStatement preparedStatement1 = connection.prepareStatement("""
+                        SELECT
+                            CategoryID
+                            , CategoryName
+                        FROM
+                            categories
+                        ORDER BY
+                            CategoryID
+                        """);
+
+                // executes query and gets the result set
+                ResultSet results = preparedStatement1.executeQuery()
+        ) {
+            // displays the result set
+            while (results.next()) {
+                System.out.printf("""
+                                
+                                Category ID: %s
+                                Category Name: %s
+                                """,
+                        results.getInt("CategoryID"),
+                        results.getString("CategoryName"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // asks user if they want to search by category id
+        System.out.print("\nDo you want to display products by category? (Yes/No) ");
+        String choice = userInput.nextLine().toLowerCase().trim();
+
+        // if yes then calls method, if no breaks to home screen
+        while (true) {
+            if (choice.equals("yes") || choice.equals("y")) {
+                displayProductByCategory();
+                break;
+            } else if (choice.equals("no") || choice.equals("n")) {
+                break;
+            } else {
+                System.out.println("Please enter either (Yes/No)!");
+                break;
+            }
+        }
+    }
+
+    // displays products depending on category ID number
+    public static void displayProductByCategory() {
+
+        // asks user for id
+        System.out.print("Enter Category ID to search: ");
+        int search = userInput.nextInt();
+
+        try (
+                // creates query with a parameter to for matching category id
+                PreparedStatement preparedStatement2 = connection.prepareStatement("""
+                        SELECT
+                            P.ProductID
+                            , P.ProductName
+                            , P.UnitPrice
+                            , P.UnitsInStock
+                            , C.CategoryID
+                        FROM
+                            products P
+                            JOIN categories C ON (P.CategoryID = C.CategoryID)
+                        WHERE
+                            C.CategoryID = ?
+                        """);
+        ) {
+            // replaces first parameter with user input
+            preparedStatement2.setInt(1, search);
+
+            // executes query and gets result set
+            try (ResultSet results2 = preparedStatement2.executeQuery()) {
+                // table header
+                System.out.print("""
+                        
+                        ID      Name                               Price   Stock   Category ID
+                        ---     --------------------------         ------  ------  -----------
+                        """);
+
+                // displays the query executed
+                while (results2.next()) {
+                    // left aligned by however many spaces to align with table header above
+                    System.out.printf("%-7d %-34s %-7.2f %-6d %-8d\n",
+                            results2.getInt("ProductID"),
+                            results2.getString("ProductName"),
+                            results2.getDouble("UnitPrice"),
+                            results2.getInt("UnitsInStock"),
+                            results2.getInt("CategoryID"));
                 }
             }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
